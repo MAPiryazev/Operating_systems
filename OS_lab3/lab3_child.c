154169cba2ece7e282567a77511ec7f1f24f978a
MAPiryazev <144619960+MAPiryazev@users.noreply.github.com>
2023-11-09 20:58:13 +0300

    removed strange code from lab3_child.c

--- OS_lab3/lab3_child.c
+++ OS_lab3/lab3_child.c
@@ -39,42 +39,6 @@ void error_checking(int result, char* error)
     }
 }
 
-int is_vowel(char symbol)
-{
-    symbol = tolower(symbol);
-    
-    if (symbol == 'a' || symbol == 'e' || symbol == 'i' || symbol == 'o' || symbol == 'u' || symbol == 'y')
-    {
-        return 1;
-    }
-    
-    return 0;
-}
-
-char* delete_vowels(char input_string[], int input_string_size)
-{
-    int output_string_size = 0;
-    int capacity = 1;
-    char* output_string = (char*) malloc(sizeof(char));
-    
-    for (int index = 0; index < input_string_size; ++index)
-    {
-        if (!is_vowel(input_string[index]))
-        {
-            output_string[output_string_size] = input_string[index];
-            ++output_string_size;
-            
-            if (output_string_size >= capacity)
-            {
-                capacity *= 2;
-                output_string = (char*) realloc(output_string, capacity * sizeof(char));
-            }
-        }
-    }
-    output_string[output_string_size] = '\0';
-    return output_string;
-}
-
 char* reverseString(char* input) {
     int length = strlen(input);
     int first_marker = 0;
